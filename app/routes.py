adbaef724eb423718c36a604184a8df19163e867
Jhordan Gabriel <gabrieljhordang@gmail.com>
2022-01-30 13:44:13 -0300

    working with HTML in flask

--- app/routes.py
+++ app/routes.py
@@ -4,4 +4,16 @@ from app import app
 @app.route('/index')
 
 def index():
-    return 'Hello world!'
+    user = {'Username':'Gabriel'}
+    
+    return '''
+<html>
+    <head> 
+        <title> Home Page - Microblog </title>
+    </head>
+        
+    <body>
+        <h1> Hello ''' + user['Username']+ '''!</h1>
+    </body>
+</html>
+'''
