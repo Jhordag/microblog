ec8353547d3a0aebb49a04c30e869c517fb7526b
Jhordan Gabriel <gabrieljhordang@gmail.com>
2022-01-30 20:58:31 -0300

    Requiring Users To Login

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,16 @@
 from app import app
 from app.models import User
-
-from flask import render_template, flash, redirect, url_for
-from flask_login import login_user, current_user, logout_user
-
 from app.forms import LoginForm
+
+from flask import render_template, flash, redirect, url_for,request
+from flask_login import login_user, current_user, logout_user, login_required
+
+from werkzeug.urls import url_parse
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'username':'Gabriel'}
-    
     posts = [
         {
             'author' : {'username':'Maria'},
@@ -23,7 +23,7 @@ def index():
         }
     ]
     
-    return render_template('index.html', title = 'Home', user=user, posts=posts)
+    return render_template('index.html', title = 'Home', posts=posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -39,7 +39,10 @@ def login():
             flash('Invalid username or password!')
             return redirect(url_for('login'))
         login_user(user, remenber=form.remember_me.data)
-        return redirect(url_for('index'))
+        next_page = redirect.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html',title='Sing In', form=form)
 
 @app.route('/logout')
