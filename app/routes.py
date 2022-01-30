198563c1faa99ef0cabc6af52ac1e061cc42affc
Jhordan Gabriel <gabrieljhordang@gmail.com>
2022-01-30 13:54:23 -0300

    Conditional Statements and html

--- app/routes.py
+++ app/routes.py
@@ -7,4 +7,4 @@ from flask import render_template
 def index():
     user = {'username':'Gabriel'}
     
-    return render_template('index.html', title = 'Home Page', user=user)
+    return render_template('index.html', user=user)
