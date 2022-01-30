059c796192964cefc69fd87a359a44a8e5149c8f
Jhordan Gabriel <gabrieljhordang@gmail.com>
2022-01-30 15:37:03 -0300

    create form templates and form views

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
 from app import app
 from flask import render_template
 
+from app.forms import LoginForm
+
 @app.route('/')
 @app.route('/index')
+
 
 def index():
     user = {'username':'Gabriel'}
@@ -20,3 +23,8 @@ def index():
     ]
     
     return render_template('index.html', title = 'Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title='Sing In', form=form)
