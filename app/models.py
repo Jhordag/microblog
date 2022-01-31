6c52ff42542216e99f743d25792caa3909ef5fe1
Jhordan Gabriel <gabrieljhordang@gmail.com>
2022-01-30 22:02:31 -0300

    Creating template and function for user registration

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64),index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='authos', lazy='dynamic')
     
     def __repr__(self):
@@ -21,6 +21,9 @@ class User(UserMixin,db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+@login.user_loader
+def loader_user(id):
+    return User.query.get(int(id))
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
@@ -30,6 +33,4 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
-@login.user_loader
-def loader_user(id):
-    return User.query.get(int(id))
+
