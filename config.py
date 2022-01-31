31913c18a57ef8f7b5cb2bbf28f6a0b4b69d9f30
Jhordan Gabriel <gabrieljhordang@gmail.com>
2022-01-31 17:29:32 -0300

    building logic for sending errors by email.

--- config.py
+++ config.py
@@ -8,3 +8,12 @@ class Config(object):
     'sqlite:///' + os.path.join(basedir, 'app.db')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_T') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASS')
+    
+    ADMINS = ['email@email.com']
