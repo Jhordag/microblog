e9db1c4469b11842b2b64f09d5f04db10d3e5ab5
Jhordan Gabriel <gabrieljhordang@gmail.com>
2022-02-01 22:09:52 -0300

    Applying the post submission rule in the app

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app, db
-from app.models import User
-from app.forms import LoginForm, RegisterForm, EditProfileForm, EmptyForm
+from app.models import User, Post
+from app.forms import (LoginForm, RegisterForm, EditProfileForm, 
+                       EmptyForm, PostForm)
 
 from flask import render_template, flash, redirect, url_for,request
 from flask_login import login_user, current_user, logout_user, login_required
@@ -9,23 +10,20 @@ from werkzeug.urls import url_parse
 
 from datetime import datetime
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/index')
 @login_required
 def index():
-    posts = [
-        {
-            'author' : {'username':'Maria'},
-            'body' : 'Beatiful day in Partland!'
-        },
-        
-        {
-            'author' : {'username':'Victor'},
-            'body' : 'The Avengers movie was so cool!'
-        }
-    ]
-    
-    return render_template('index.html', title = 'Home', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+       
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title = 'Home', posts=posts, form=form)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
